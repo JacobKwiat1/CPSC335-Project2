8d12b28174df8694257e4a37d6a87e1adfdf4b0d
Mike G <Mike G@DESKTOP-DDT8VSO>
2026-03-13 19:30:00 -0700

    added wall and non visited checks

--- DFS.py
+++ DFS.py
@@ -1,4 +1,18 @@
 from time import perf_counter
+def neighbor_added_on_stack(maze , current_spot, Rows, Columns, direct, visited, stack, parent):
+    R_row, C_column = current_spot
+
+    for direct_r, direct_c in direct:
+        new_r = R_row + direct_r
+        new_c = C_column + direct_c
+        next_spot = (new_r,new_c)
+
+        if 0 <= new_r and 0 <= new_c <Columns:
+            if maze[new_r][new_c] != '#' and next_spot not in visited:
+                stack.append(next_spot)
+                visited.add(next_spot)
+                parent[next_spot] = current_spot
+            
 def DFS_maze(maze):
     Rows = len(maze)
     Columns = len(maze[0])
@@ -21,7 +35,7 @@ def DFS_maze(maze):
     parent = {} 
     visited_count = 0
 
-    directions =[(-1,0),(1,0), (0,-1), (0,1)]
+    direct =[(-1,0),(1,0), (0,-1), (0,1)]
 
     while stack:
         current_spot = stack.pop()
@@ -32,7 +46,7 @@ def DFS_maze(maze):
             while current_spot != start:
                 path.append(start)
                 path.reverse()
-
-            runtime = perf_counter() - start_time
-            return path, visited_count, runtime
+        neighbor_added_on_stack(maze , current_spot, Rows, Columns, direct, visited, stack, parent)
+    runtime = perf_counter() - start_time
+    return path, visited_count, runtime
           
